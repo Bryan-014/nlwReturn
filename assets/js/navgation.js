75260e77f01e628e10c4cc143906a57e8e173926
Bryan-014 <bryangustavo201904@gmail.com>
2022-05-06 23:45:16 -0300

    fixes

--- assets/js/navgation.js
+++ assets/js/navgation.js
@@ -2,18 +2,49 @@ let menu = {
   navClassList: navigation.classList,
   navigation: {
     addClass: () => menu.navClassList.add('scroll'),
-    removeClass: () => menu.navClassList.remove('scroll')
+    removeClass: () => menu.navClassList.remove('scroll'),
+    showNavOnScroll: () => {
+      if (scrollY != 0) menu.navigation.addClass();
+      else menu.navigation.removeClass();
+    },
+    activateMenuAtCurrentSection: section => {
+      const targetLine = scrollY + innerHeight / 2;
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const menuElement = document.querySelector(
+        `.menu a[href*=${section.getAttribute('id')}]`
+      );
+      menuElement.classList.remove('active');
+      if (
+        targetLine >= sectionTop &&
+        !(sectionTop + sectionHeight <= targetLine)
+      ) {
+        menuElement.classList.add('active');
+      }
+    }
   },
   bodyClassList: document.body.classList,
   expanded: {
     open: () => menu.bodyClassList.add('menu-expanded'),
     close: () => menu.bodyClassList.remove('menu-expanded')
+  },
+  btnBackTopClassList: backToTopButton.classList,
+  backToTopButton: {
+    show: () => menu.btnBackTopClassList.add('show'),
+    hidden: () => menu.btnBackTopClassList.remove('show'),
+    onScrollEvent: () => {
+      if (scrollY > 550) menu.backToTopButton.show();
+      else menu.backToTopButton.hidden();
+    }
   }
 };
 
 document.addEventListener('scroll', () => {
-  if (scrollY != 0) menu.navigation.addClass();
-  else menu.navigation.removeClass();
+  menu.navigation.showNavOnScroll();
+  menu.backToTopButton.onScrollEvent();
+  menu.navigation.activateMenuAtCurrentSection(home);
+  menu.navigation.activateMenuAtCurrentSection(services);
+  menu.navigation.activateMenuAtCurrentSection(about);
 });
 
 ScrollReveal({
@@ -35,3 +66,8 @@ ScrollReveal({
   #contact .content,
   footer
 `);
+
+menu.backToTopButton.onScrollEvent();
+menu.navigation.activateMenuAtCurrentSection(home);
+menu.navigation.activateMenuAtCurrentSection(services);
+menu.navigation.activateMenuAtCurrentSection(about);
